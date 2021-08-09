5b39c1ce9dc4258f7b90477b06adf67e418baf00
Renov <renov.jung@gmail.com>
2021-08-09 22:43:46 +0900

    Logo and icon

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps extends SvgProps {
 
 const Logo: React.FC<LogoProps> = () => {
   return (
-    <img alt="HoneyFarmFinance" src="/images/polyhoney/honeyfarm_dark.png" style={{width: "auto", height: "30px"}} />
+    <img alt="HoneyFarmFinance" src="/images/honeybear/main_logo.png" style={{width: "auto", height: "30px"}} />
   );
 };
 
