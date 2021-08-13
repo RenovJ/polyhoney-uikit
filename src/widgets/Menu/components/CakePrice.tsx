9313015ab056293db6ca6a998bf9cf56b845da0f
Renov <renov.jung@gmail.com>
2021-08-13 20:40:41 +0900

    Changed Price link to bear token

--- src/widgets/Menu/components/CakePrice.tsx
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -23,7 +23,7 @@ const PriceLink = styled.a`
 
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
   return cakePriceUsd ? (
-    <PriceLink href="https://bscscan.com/address/0xfa363022816abf82f18a9c2809dcd2bb393f6ac5" target="_blank">
+    <PriceLink href="https://bscscan.com/address/0xc3EAE9b061Aa0e1B9BD3436080Dc57D2d63FEdc1" target="_blank">
       <PancakeRoundIcon width="24px" mr="8px" />
       <Text color="textMenu" bold>{`$${cakePriceUsd.toFixed(2)}`}</Text>
     </PriceLink>
