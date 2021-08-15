4fc2d838bb3ef48b8d828f6b2b7ac6a80cdd469c
Renov <renov.jung@gmail.com>
2021-08-15 14:33:13 +0900

    Changed Rugdoc link url

--- src/widgets/Menu/components/Panel.tsx
+++ src/widgets/Menu/components/Panel.tsx
@@ -40,7 +40,7 @@ const Panel: React.FC<Props> = (props) => {
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
-      <a href={"https://rugdoc.io"} target="_blank">
+      <a href={"https://rugdoc.io/project/honey-farm/"} target="_blank">
         <img width="100%" src="images/rugdoc.png" />
       </a>
       <PanelFooter {...props} />
