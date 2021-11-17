9ffcaace0bc7a2d6bc060cd9c1fd653e1b77f915
Jaewoo Kim <kjw3898@gmail.com>
2021-11-17 22:58:05 +0900

    fix: bigger ticker and price on menu footer

--- src/widgets/Menu/components/CakePrice.tsx
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -24,11 +24,11 @@ const PriceLink = styled.a`
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
   return cakePriceUsd ? (
     <PriceLink href="https://snowtrace.io/token/0xB669c71431bc4372140bC35Aa1962C4B980bA507" target="_blank">
-      <PancakeRoundIcon width="24px" mr="8px" />
-      <Text color="textMenu" bold>{`$${cakePriceUsd.toFixed(2)}`}</Text>
+      <PancakeRoundIcon width="40px" mr="8px" />
+      <Text color="textMenu" bold fontSize={'20px'}>{`$${cakePriceUsd.toFixed(2)}`}</Text>
     </PriceLink>
   ) : (
-    <Skeleton width={80} height={24} />
+    <Skeleton width={80} height={40} />
   );
 };
 
--- src/widgets/Menu/components/SocialLinks.tsx
+++ src/widgets/Menu/components/SocialLinks.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { SvgProps } from "../../../components/Svg";
 import Flex from "../../../components/Box/Flex";
 import Dropdown from "../../../components/Dropdown/Dropdown";
@@ -7,28 +8,37 @@ import * as IconModule from "../icons";
 import { socials } from "../config";
 
 const Icons = (IconModule as unknown) as { [key: string]: React.FC<SvgProps> };
-
 const SocialLinks: React.FC = () => (
-  <Flex>
+  <Flex flexWrap={'wrap'} ml={'10px'}>
     {socials.map((social, index) => {
       const Icon = Icons[social.icon];
+      const HoverIcon = styled(Icon)`
+        transition: background-color 0.2s, opacity 0.2s;
+        &:hover:not(:disabled):not(.pancake-button--disabled):not(.pancake-button--disabled):not(:active) {
+          opacity: 0.65;
+        }
+      `
       const iconProps = { width: "24px", color: "textMenu", style: { cursor: "pointer" } };
       const mr = index < socials.length - 1 ? "6px" : 0;
       if (social.items) {
         return (
-          <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} />}>
-            {social.items.map((item) => (
-              <Link external key={item.label} href={item.href} aria-label={item.label} color="text">
-                {item.label}
-              </Link>
-            ))}
-          </Dropdown>
+          <div style={{flex:'1 1 45%', paddingLeft: '9px'}}>
+            <Dropdown key={social.label} position="top" target={<HoverIcon {...iconProps} mr={mr} />}>
+              {social.items.map((item) => (
+                <Link external key={item.label} href={item.href} aria-label={item.label} color="text">
+                  {item.label}
+                </Link>
+              ))}
+            </Dropdown>
+          </div>
         );
       }
       return (
-        <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
-          <Icon {...iconProps} />
-        </Link>
+        <div style={{flex:'1 1 45%', paddingLeft: '9px'}}>
+          <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr} pl={social.label=='Twitter' ? '2px': '0px'}>
+            <HoverIcon {...iconProps} />
+          </Link>
+        </div>
       );
     })}
     
