c243d02c009ab69d850e9677f53614a72935511d
JaewooKim <kjw3898@gmail.com>
2022-03-11 21:11:35 +0900

    fix: key error fix

--- src/widgets/Menu/components/SocialLinks.tsx
+++ src/widgets/Menu/components/SocialLinks.tsx
@@ -9,23 +9,40 @@ import { socials } from "../config";
 
 const Icons = (IconModule as unknown) as { [key: string]: React.FC<SvgProps> };
 const SocialLinks: React.FC = () => (
-  <Flex flexWrap={'wrap'} ml={'10px'}>
+  <Flex flexWrap={"wrap"} ml={"10px"}>
     {socials.map((social, index) => {
       const Icon = Icons[social.icon];
-      const iconProps = { width: "24px", color: "textMenu", style: { cursor: "pointer" } };
+      const iconProps = {
+        width: "24px",
+        color: "textMenu",
+        style: { cursor: "pointer" },
+      };
       const HoverIcon = styled(Icon)`
         transition: background-color 0.2s, opacity 0.2s;
         &:hover:not(:disabled):not(.pancake-button--disabled):not(.pancake-button--disabled):not(:active) {
           opacity: 0.65;
         }
-      `
+      `;
       const mr = index < socials.length - 1 ? "6px" : 0;
       if (social.items) {
         return (
-          <div style={{flex:'1 1 45%', paddingLeft: '9px'}}>
-            <Dropdown key={social.label} position="top" target={<HoverIcon {...iconProps} mr={mr} />}>
+          <div
+            key={social.label}
+            style={{ flex: "1 1 45%", paddingLeft: "9px" }}
+          >
+            <Dropdown
+              key={social.label}
+              position="top"
+              target={<HoverIcon {...iconProps} mr={mr} />}
+            >
               {social.items.map((item) => (
-                <Link external key={item.label} href={item.href} aria-label={item.label} color="menuBackground">
+                <Link
+                  external
+                  key={item.label}
+                  href={item.href}
+                  aria-label={item.label}
+                  color="menuBackground"
+                >
                   {item.label}
                 </Link>
               ))}
@@ -34,8 +51,14 @@ const SocialLinks: React.FC = () => (
         );
       }
       return (
-        <div style={{flex:'1 1 45%', paddingLeft: '9px'}}>
-          <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr} pl={social.label=='Twitter' ? '2px': '0px'}>
+        <div key={social.label} style={{ flex: "1 1 45%", paddingLeft: "9px" }}>
+          <Link
+            external
+            href={social.href}
+            aria-label={social.label}
+            mr={mr}
+            pl={social.label == "Twitter" ? "2px" : "0px"}
+          >
             <HoverIcon {...iconProps} />
           </Link>
         </div>
